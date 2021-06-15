cdc59cd72f4936ef128e918ef490f2b3b1b447bc
glavrab <g2NUYeda32200>
2021-06-16 00:35:03 +0300

    feature: Create WikiSearcher method to look for categorises titles and subcategories

--- shared/constants.py
+++ shared/constants.py
@@ -7,6 +7,24 @@ class Wiki(enum.Enum):
 
 
 class Codes(enum.Enum):
-    """Codes for understimation of required search"""
+    """Codes for underestimation of required search"""
     SEARCH_RANDOM_PAGE_TITLE = 100
     SEARCH_REQUIRED_PAGE = 200
+
+
+class SearchedObjectCategories(enum.Enum):
+    """Objects category to search in wiki"""
+    PHYSICS = 'Category:Physics'
+    CHEMISTRY = 'Category:Chemistry'
+    HISTORY = 'Category:History'
+    BIOLOGY = 'Category:Biology'
+    IT = 'Category:Information and communications technology'
+
+
+class SearchedObjectTypes(enum.Enum):
+    """Existing object types to search in wiki"""
+    TITLE = 'title'
+    PAGE = 'page'
+    SUBCATEGORY = 'subcat'
+    FILE = 'file'
+    CATEGORY_MEMBERS = 'categorymembers'
--- wiki_searcher/utilities.py
+++ wiki_searcher/utilities.py
@@ -1,11 +1,40 @@
+import ujson
+import typing
+import aiohttp
+from shared.constants import Wiki, SearchedObjectTypes
 
-def get_object_info(data: dict[str]) -> str:
-    """Parse json file to get required info"""
+
+async def process_searching(search_settings: dict[typing.Union[str, int]],
+                            task: str) -> typing.Union[str, tuple[list[str], list[str]]]:
+    """Get object info from required page"""
+    async with aiohttp.ClientSession(json_serialize=ujson.dumps) as session:
+        response = await session.get(Wiki.API_URL.value, params=search_settings)
+        await session.close()
+        if task != SearchedObjectTypes.CATEGORY_MEMBERS.value:
+            return get_object_info_for_one_page(await response.json())
+        return get_titles_and_categories(await response.json())
+
+
+def get_object_info_for_one_page(data: dict[str]) -> str:
+    """Parse json file to get reqired page's title or info"""
     try:
         pages_info = list(data['query']['pages'].values())
-        object_description = pages_info[0]['extract']
+        object_description = pages_info[0]['extract']  # only 1 value represents because we look for 1 title
         return object_description
     except KeyError:
-        object_info = data['query']['random'][0]
+        object_info = data['query']['random'][0]  # same because we get 1 random page
         title = object_info['title']
         return title
+
+
+def get_titles_and_categories(data: dict[str]) -> tuple[list[str], list[str]]:
+    """Parse json file for titles to insert into db or categories"""
+    titles = []
+    categories = []
+    search_results = data['query']['categorymembers']
+    for result in search_results:
+        if result['ns'] == 0:  # Means that result is page other is category
+            titles.append(result)
+        categories.append(result)
+    parsed_data = (titles, categories)
+    return parsed_data
