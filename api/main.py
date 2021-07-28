587e1add7850a189a32dc16f9d6daebeec745a25
aleksey <alek.bar.ru@yandex.ru>
2021-07-29 00:13:55 +0300

    feature: Add loggers to registration handler

--- api/main.py
+++ api/main.py
@@ -1,11 +1,21 @@
-from aiohttp import web
-from loguru import logger
-from api.utilities import register_user
 import ujson
+from aiohttp import web
+from aiohttp_session import setup, get_session
+from loguru import logger
+
+from api.utilities import (
+    register_user,
+    login_user,
+    get_random_fact,
+    check_if_user_logged_in,
+    create_redis_storage,
+    get_random_rated_fact_info,
+    process_rating,
+)
+from database.models import connect_to_db, db
+from database.utilities import apply_migrations
 from shared.exceptions import PasswordError, LoginError
 from shared.project_settings import settings
-from database.utilities import apply_migrations
-from database.models import connect_to_db, db
 
 
 app_route = web.RouteTableDef()
@@ -14,11 +24,14 @@ app_route = web.RouteTableDef()
 @app_route.post('/registration')
 async def register(request: web.Request):
     data = await request.json(loads=ujson.loads)
+    logger.debug(f'User: {data["username"]} is trying to register')
     try:
         response = await register_user(data)
+        logger.debug(f'Successful registration by user: {data["username"]}')
         return web.json_response(response, dumps=ujson.dumps)
     except (LoginError, PasswordError) as error:
-        response = {'error': error}
+        response = {'error': str(error)}
+        logger.debug(f'Registration error: {error}, user: {data["username"]}')
         return web.json_response(response, dumps=ujson.dumps)
 
 
