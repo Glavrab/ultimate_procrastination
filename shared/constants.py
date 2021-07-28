5e90056fe6035683eed7a3334c173a587e8b9c5d
aleksey <alek.bar.ru@yandex.ru>
2021-07-28 23:56:26 +0300

    feature: Add constants for response result, info types, urls and current bot task

--- shared/constants.py
+++ shared/constants.py
@@ -1,6 +1,45 @@
 import enum
 
 
+class CurrentTask(enum.Enum):
+    """Current task processing by user"""
+    LOGIN_PAGE = 'Login page'
+    REGISTRATION = 'Registration'
+    LOGIN = 'Login'
+    GETTING_INFO = 'Getting info'
+
+
+class URL(enum.Enum):
+    """Urls for accessing API"""
+    REGISTER = 'procrastination_web:8000/registration'
+    LOGIN = 'procrastination_web:8000/login'
+    RANDOM_FACT = 'procrastination_web:8000/random_fact'
+    RANDOM_RATED_FACT = 'procrastination_web:8000/random_rated_fact'
+    RATE_FACT = 'procrastination_web:8000/rate_fact'
+
+
+class InfoTypeId(enum.Enum):
+    """Info category id"""
+    PHYSICS = 0
+    CHEMISTRY = 1
+    BIOLOGY = 2
+    HISTORY = 3
+    IT = 4
+
+
+class RateCommand(enum.Enum):
+    """Rating commands"""
+    LIKE = 'Like'
+    DISLIKE = 'Dislike'
+    NEXT = 'Next'
+    MORE_INFO = 'More info'
+
+
+class Codes(enum.Enum):
+    """Response result codes"""
+    SUCCESS = 500
+
+
 class PasswordErrorMessage(enum.Enum):
     """Password errors messages"""
     UNMATCHED_PASSWORD = 'Typed passwords do not match up'
@@ -22,7 +61,7 @@ class Wiki(enum.Enum):
 
 
 class SearchedObjectCategories(enum.Enum):
-    """Objects category to search in wiki"""
+    """Category objects to search in wiki"""
     PHYSICS = 'Category:Physics'
     CHEMISTRY = 'Category:Chemistry'
     HISTORY = 'Category:History'
