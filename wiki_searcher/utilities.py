9c2556b1369cac527a36c33bc4bd7b9964bb068a
aleksey <alek.bar.ru@yandex.ru>
2021-07-28 23:43:52 +0300

    style: Change style

--- wiki_searcher/utilities.py
+++ wiki_searcher/utilities.py
@@ -10,21 +10,21 @@ async def process_searching(search_settings: dict[typing.Union[str, int]],
     async with aiohttp.ClientSession(json_serialize=ujson.dumps) as session:
         response = await session.get(Wiki.API_URL.value, params=search_settings)
         await session.close()
-        if task != SearchedObjectTypes.CATEGORY_MEMBERS.value:
-            return get_object_info_for_one_page(await response.json())
-        return get_titles_and_categories(await response.json())
+    if task != SearchedObjectTypes.CATEGORY_MEMBERS.value:
+        return get_object_info_for_one_page(await response.json())
+    return get_titles_and_categories(await response.json())
 
 
 def get_object_info_for_one_page(data: dict[str]) -> str:
     """Parse json file to get reqired page's title or info"""
-    try:
+    result = data['query'].setdefault('pages')
+    if result:
         pages_info = list(data['query']['pages'].values())
         object_description = pages_info[0]['extract']  # only 1 value represents because we look for 1 title
         return object_description
-    except KeyError:
-        object_info = data['query']['random'][0]  # same because we get 1 random page
-        title = object_info['title']
-        return title
+    object_info = data['query']['random'][0]  # same because we get 1 random page
+    title = object_info['title']
+    return title
 
 
 def get_titles_and_categories(data: dict[str]) -> tuple[list[str], list[str]]:
@@ -36,5 +36,4 @@ def get_titles_and_categories(data: dict[str]) -> tuple[list[str], list[str]]:
         if result['ns'] == 0:  # Means that result is page other is category
             titles.append(result['title'])
         categories.append(result['title'])
-    parsed_data = (titles, categories)
-    return parsed_data
+    return titles, categories
