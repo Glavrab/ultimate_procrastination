69fa486844b640d3aff9e31b8e2b699951b65080
aleksey <alek.bar.ru@yandex.ru>
2021-08-02 00:29:57 +0300

    fix: Initialize super for custom exceptions

--- shared/exceptions.py
+++ shared/exceptions.py
@@ -1,19 +1,20 @@
-class Error(Exception):
+class ProcrastinationError(Exception):
 
     def __init__(self, error_message: str):
         self.error_message = error_message
+        super(ProcrastinationError, self).__init__(error_message)
 
     def __str__(self):
         return f'Something is wrong with your data: {self.error_message}'
 
 
-class PasswordError(Error):
+class PasswordError(ProcrastinationError):
     pass
 
 
-class LoginError(Error):
+class LoginError(ProcrastinationError):
     pass
 
 
-class AuthorizationError(Error):
+class AuthorizationError(ProcrastinationError):
     pass
